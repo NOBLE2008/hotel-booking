ab30f611380b3bad468c523fc5cb90b6b0f734dc
Noble Benjamin <nobleben2008@gmail.com>
2024-08-19 13:53:27 +0100

    Used the useSettings in the updateSettingsForm

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,21 +1,23 @@
 import Form from '../../ui/Form';
 import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
+import useSettings from './useSettings';
 
 function UpdateSettingsForm() {
+  const {data: {minBookingLength, maxBookingLength, maxGuestPerBooking, breakFastPrice}, isLoading} = useSettings()
   return (
     <Form>
       <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+        <Input type='number' id='min-nights' defaultValue={minBookingLength}/>
       </FormRow>
       <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+        <Input type='number' id='max-nights' defaultValue={maxBookingLength}/>
       </FormRow>
       <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+        <Input type='number' id='max-guests' defaultValue={maxGuestPerBooking}/>
       </FormRow>
       <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+        <Input type='number' id='breakfast-price' defaultValue={breakFastPrice}/>
       </FormRow>
     </Form>
   );
