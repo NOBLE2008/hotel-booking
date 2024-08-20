53c4a8283c1ea26d66945608fe881ba27280d8c5
Noble Benjamin <nobleben2008@gmail.com>
2024-08-20 17:31:15 +0100

    Extracted the close modal function to Custom hook

---
+++ src/hooks/useCloseModal.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useRef } from 'react'
+
+export default function useCloseModal(close) {
+    const ref = useRef();
+
+    useEffect(
+      function () {
+        function handleClick(e) {
+          if (ref.current && !ref.current.contains(e.target)) {
+            close();
+          }
+        }
+        document.addEventListener("click", handleClick, true);
+  
+        return () => {
+          document.removeEventListener("click", handleClick, true);
+        };
+      },
+      [close]
+    );
+  return ref
+}
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -82,23 +82,7 @@ function Open({ children, open: windowOpenName }) {
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
 
-  const ref = useRef();
-
-  useEffect(
-    function () {
-      function handleClick(e) {
-        if (ref.current && !ref.current.contains(e.target)) {
-          close();
-        }
-      }
-      document.addEventListener("click", handleClick, true);
-
-      return () => {
-        document.removeEventListener("click", handleClick, true);
-      };
-    },
-    [close]
-  );
+ 
   if (openName !== name) return null;
 
   return createPortal(
